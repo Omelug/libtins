7cfea95236b0c2fde4c4b4882d193a461656cbaf
Vit Janecek <xjanecv00@vutbr.cz>
2025-10-03 08:39:29 +0200

    ignore SAE for now, fix

--- include/tins/dot11/sae_dot11_auth.h
+++ include/tins/dot11/sae_dot11_auth.h
@@ -13,10 +13,7 @@ public:
     static const PDUType pdu_flag = PDU::DOT11_AUTH;
     SAEDot11Authentication(const uint8_t* buffer, uint32_t total_sz);
     uint8_t sae_message_type() const { return sae_message_type_; }
-    uint16_t group_id() const { return group_id_; }
     const std::vector<uint8_t>& anti_clogging_token() const { return anti_clogging_token_; }
-    const std::vector<uint8_t>& scalar() const { return scalar_; }
-    const std::vector<uint8_t>& finite_field_element() const { return finite_field_element_; }
     PDUType pdu_type() const { return pdu_flag; }
     SAEDot11Authentication* clone() const { return new SAEDot11Authentication(*this); }
 private:
